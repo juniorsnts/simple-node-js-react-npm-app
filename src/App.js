5e5ef678076b603eb6dfd089f2166983e80c5a7a
Junior santos <juniorsnts123@gmail.com>
2019-11-07 08:15:03 -0300

    ajuste jenkinsfile

--- src/App.js
+++ src/App.js
@@ -13,9 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    export default App;
